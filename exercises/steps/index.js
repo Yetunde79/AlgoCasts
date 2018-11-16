9b6db269abef39f95121e4babbc6bb00b223ab3e
Yetunde Sola-Adebayo <ysolaadebayo@gmail.com>
2018-11-16 16:25:20 -0600

    steps pt 2

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,18 +17,37 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-    for (let col = 0; col < n; col++) {
-      if (col <= row) {
-        stair += "#";
-      } else {
-        stair += " ";
-      }
-    }
-    console.log(stair);
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " "; //add space
+//       }
+//     }
+//     console.log(stair); //print it n number of times
+//   }
+// }
+
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return; //exits the function
   }
+
+  if (n === stair.length) {
+    //if true, go to next row
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair); //recursion
 }
 
 module.exports = steps;
