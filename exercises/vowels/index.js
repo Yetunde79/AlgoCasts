9192338087e07b074fcc1a914f14521247a9e280
Yetunde Sola-Adebayo <ysolaadebayo@gmail.com>
2018-11-21 22:16:47 -0600

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   let num = 0;
+//   let vowel = ["a", "e", "i", "o", "u"];
+
+//   for (let char of str.toLowerCase()) {
+//     if (vowel.includes(char)) {
+//       num++;
+//     }
+//   }
+//   return num;
+// }
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi); //returns true if it  matches
+  //regex: g makes sure we dont stop at first match, i means it's case insensitive
+  return matches ? matches.length : 0; //if u find vowel return lenth else return 0
+}
 
 module.exports = vowels;
